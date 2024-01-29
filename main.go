c2788cda5f44d8391108c8b4bc673837f61950f9
5HT2 <git@frogg.ie>
2024-01-29 03:46:54 -0800

    impr: Rename `-twitter` → `t`

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 var (
 	snowflakeInput = flag.Int64("s", 0, "Snowflake to parse")
 	formatStr      = flag.String("f", "%u - %p", "Format string, options: %u (unix ms), %p (RFC3339), %t (<t:unix>), %t:S (<t:unix:style>)")
-	twitterEpoch   = flag.Bool("twitter", false, "Set to parse Twitter snowflake instead of Discord")
+	twitterEpoch   = flag.Bool("t", false, "Set to parse Twitter snowflake instead of Discord")
 )
 
 func main() {
